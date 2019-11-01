c19df03b1895ee2779c4f9c9dfadc50c9ccbaeee
wj <18351867657@163.com>
2019-11-01 17:09:09 +0800

    1

---
+++ frame/frame_web/src/main/java/com/dj/cn/controller/page/TestController.java
@@ -0,0 +1,9 @@
+package com.dj.cn.controller.page;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test/")
+public class TestController {
+}
